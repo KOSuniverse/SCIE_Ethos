68312272c68c73fc6c2b32531b3760eab98291a7
KOSuniverse <danskemp@yahoo.com>
2025-07-21 11:08:04 -0500

    Delete utils/__init__.py

--- utils/__init__.py
+++
@@ -1,9 +0,0 @@
-# Utils package for Jarvis Knowledge Base Assistant
-
-# Optional: Expose commonly used functions at package level
-# Uncomment below if you want cleaner imports like: from utils import get_embedding
-# from .llm_client import get_embedding, cosine_similarity
-# from .gdrive import download_file, list_all_supported_files
-# from .metadata import load_metadata, save_metadata
-
-# For now, keeping it simple with explicit imports in main.py
